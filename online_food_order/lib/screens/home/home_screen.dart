bd3e8c1b2431b06d8d065830f5754c64e3b4599a
THEVINDU RANSARA RATHNAYAKA <97075043+trsrathnayaka@users.noreply.github.com>
2023-10-08 19:30:07 +0530

    new

--- online_food_order/lib/screens/home/home_screen.dart
+++ online_food_order/lib/screens/home/home_screen.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:online_food_order/constants.dart';
 
+import 'components/header.dart';
 import 'components/menu.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -29,63 +30,104 @@ class _HomeScreenState extends State<HomeScreen> {
                   constraints: BoxConstraints(maxWidth: kMaxWidth),
                   child: Column(
                     children: [
+                      Header(),
+                      SizedBox(
+                        height: 50,
+                      ),
                       Row(
                         children: [
-                          Text(
-                            "FoodZone",
-                            style: TextStyle(
-                                fontSize: 25.0,
-                                fontWeight: FontWeight.w900,
-                                color: kSecondaryColor),
-                          ),
-                          Spacer(),
-                          //menu
-                          HeaderWebMenu(),
-                          Spacer(),
                           Expanded(
-                            child: Container(
-                              padding: EdgeInsets.only(left: 10, right: 10),
-                              height: 50,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                border: Border.all(
-                                    color: Colors.grey.withOpacity(0.3)),
-                              ),
-                              child: TextFormField(
-                                decoration: InputDecoration(
-                                    prefixIcon: Icon(
-                                      Icons.search,
-                                      color: Colors.black,
-                                    ),
-                                    hintText: "Search",
-                                    hintStyle: TextStyle(
-                                        fontWeight: FontWeight.w600,
-                                        fontSize: 14),
-                                    focusedBorder: UnderlineInputBorder(
-                                        borderSide: BorderSide.none),
-                                    enabledBorder: UnderlineInputBorder(
-                                        borderSide: BorderSide.none)),
-                              ),
+                            flex: 3,
+                            child: Column(
+                              // ignore: prefer_const_literals_to_create_immutables
+                              children: [
+                                Text(
+                                  "Eat today",
+                                  style: TextStyle(
+                                      fontSize: 56,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  "Live another day",
+                                  style: TextStyle(
+                                    fontSize: 56,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  "how to make complete responsive food / restaurant website design flutter",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Colors.black54,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Container(
+                                  padding: EdgeInsets.only(left: 10, right: 10),
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    border: Border.all(
+                                        color: Colors.grey.withOpacity(0.3)),
+                                  ),
+                                  child: TextFormField(
+                                    decoration: InputDecoration(
+                                      suffixIcon: Icon(
+                                          Icons.adjust_rounded,
+                                          color: kSecondaryColor,
+                                        ),
+                                        hintText:
+                                            "Search your favorite food item",
+                                        hintStyle: TextStyle(
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 14),
+                                        focusedBorder: UnderlineInputBorder(
+                                            borderSide: BorderSide.none),
+                                        enabledBorder: UnderlineInputBorder(
+                                            borderSide: BorderSide.none)),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Row(
+                                  children: [
+                                   Expanded(
+                                     child: MaterialButton(
+                                      height: 50,
+                                      color: kSecondaryColor,
+                                   
+                                      onPressed: () {},child: Text("Delivery"),),
+                                   ),
+                                    Text("or"),
+                                    Expanded(
+                                      child: OutlinedButton(onPressed: () {}, child: Text("Pick Up"),style: OutlinedButton.styleFrom(
+                                        
+                                        side: BorderSide(color: kSecondaryColor)
+                                      ),),
+                                    )
+                                  ],
+                                )
+                              ],
                             ),
                           ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Container(
-                            height: 45,
-                            width: 45,
-                            decoration: BoxDecoration(
-                              color: kSecondaryColor,
-                              borderRadius: BorderRadius.circular(8.0),
-                            ),
-                            child: Icon(
-                              Icons.shopping_bag_outlined,
-                              color: Colors.white,
-                              size: 22,
+                          Expanded(
+                            flex: 2,
+                            child: Column(
+                              children: [
+                                Image.asset("assets/images/banner.png"),
+                              ],
                             ),
                           )
                         ],
-                      ),
+                      )
                     ],
                   ),
                 )
